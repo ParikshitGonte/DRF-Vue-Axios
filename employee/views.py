fd912f1852b92fe56eb07ead4ac72c6315271955
unknown <parikshitgonte10@gmail.com>
2021-01-06 21:49:54 +0530

    django serializers example with vue

--- employee/views.py
+++ employee/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from .models import Employee
+from django.core import serializers
 
 def EmployeeView(request):
-    employees=Employee.objects.all()
-    return render(request,'detail.html',{'employees':employees})
+    employee=serializers.serialize("json",Employee.objects.all())
+    return render(request,'detail.html',{'employee':employee})
 
 def EmployeeView1(request):
-    employees=Employee.objects.all()
-    return render(request,'demo.html',{'employees':employees})
+    employee=serializers.serialize("json",Employee.objects.all())
+    return render(request,'demo.html',{'employee':employee})
 
 def RestView(request):
     return render(request,'api_demo.html')
